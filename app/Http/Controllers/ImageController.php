95ed1f076dee246060543a7729b375c7d5937a66
Meder <medalon07@gmail.com>
2022-09-28 13:31:06 +0600

    image upload, show, delete tutorial lesson

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+use App\Http\Requests\ImageRequest;
+
+class ImageController extends Controller
+{
+    public function index()
+    {
+        $images = Image::published()->latest()->paginate(15);
+
+        return view('image.index', compact('images'));
+    }
+
+    public function show(Image $image)
+    {
+        return view('image.show', compact('image'));
+    }
+
+    public function create()
+    {
+        return view('image.create');
+    }
+
+    public function store(ImageRequest $request)
+    {
+        Image::create($request->getData());
+        return to_route('images.index')->with('message', "Image has been uploaded sucessfully");
+    }
+
+    public function edit($image)
+    {
+        return view('image.edit', compact('image'));
+    }
+
+    public function update(Image $image, ImageRequest $request)
+    {
+        $image->update($request->getData());
+        return to_route('images.index')->with('message', "Image has been updated sucessfully");
+    }
+
+    public function destroy(Image $image)
+    {
+        $image->delete();
+        return to_route('images.index')->with('message', "Image has been removed sucessfully");
+    }
+}
---
+++ app/Http/Requests/ImageRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Image;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        if ($this->method() == 'PUT') {
+            return [
+                'title' => 'required'
+            ];
+        }
+
+        return [
+            'file' => 'required|image',
+            'title' => 'nullable'
+        ];
+    } 
+
+    public function getData()
+    { 
+        $data = $this->validated() + [
+            'user_id' => 1 // $this->user()->id
+        ];
+
+        if($this->hasFile('file'))
+        {
+            $directory = Image::makeDirectory();
+
+            $data['file'] = $this->file->store($directory);
+            $data['dimension'] = Image::getDimension($data['file']);
+        }
+
+    
+
+        return $data;
+
+    }
+
+    
+}
